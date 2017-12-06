9a9b5b62f42dec2c22b977bb00311e6c63db76ed
Felix Klauke <info@felix-klauke.de>
2017-12-06 18:47:17 +0100

    Add rest management interface.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ server/src/main/kotlin/de/d3adspace/mercantor/server/rest/JerseyRestManager.kt
@@ -0,0 +1,19 @@
+package de.d3adspace.mercantor.server.rest
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+class JerseyRestManager : RestManager {
+
+    override fun startService() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun stopService() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isServiceRunning(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
