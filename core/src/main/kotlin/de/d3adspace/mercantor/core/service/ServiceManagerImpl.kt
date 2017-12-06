a8d307f88bf26a769de885b2066f3806a079db06
Felix Klauke <info@felix-klauke.de>
2017-12-06 19:53:13 +0100

    Feature/service (#63)
    * Add javadoc for default server impl and make the repository more or less thread safer than before.
    
    * Clean the whole project and prepare for rework.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Remove unused modules.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add gradle support.
    
    * Add upcoming module configs.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add basic structure of core module.
    
    * Add basic behavior definition.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add test for mercantor factory.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add test dependencies and configure test folders.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add bootstrap mercantor server.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add basic service data model.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add basic service management delegation.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ core/src/main/kotlin/de/d3adspace/mercantor/core/service/ServiceManagerImpl.kt
@@ -0,0 +1,16 @@
+package de.d3adspace.mercantor.core.service
+
+class ServiceManagerImpl : ServiceManager {
+
+    override fun getServices(): Set<Service> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun invalidate(service: Service) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun register(service: Service) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
