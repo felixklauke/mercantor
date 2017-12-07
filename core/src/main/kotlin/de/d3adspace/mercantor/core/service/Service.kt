e4d96a460fd4688e9576100799dfa4ae88367528
Felix Klauke <info@felix-klauke.de>
2017-12-07 08:34:01 +0100

    Add service id assignment and vip address based querying.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- core/src/main/kotlin/de/d3adspace/mercantor/core/service/Service.kt
+++ core/src/main/kotlin/de/d3adspace/mercantor/core/service/Service.kt
@@ -1,7 +1,13 @@
 package de.d3adspace.mercantor.core.service
 
+import java.util.*
+
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
 interface Service {
+
+    fun setId(serviceId: UUID)
+
+    fun getVipAddress(): String
 }
--- core/src/main/kotlin/de/d3adspace/mercantor/core/service/ServiceManagerImpl.kt
+++ core/src/main/kotlin/de/d3adspace/mercantor/core/service/ServiceManagerImpl.kt
@@ -1,15 +1,20 @@
 package de.d3adspace.mercantor.core.service
 
 import de.d3adspace.mercantor.core.heartbeat.HeartBeat
+import java.util.*
 
 class ServiceManagerImpl : ServiceManager {
+
+    private val services = hashMapOf<UUID, Service>()
 
     override fun getService(vipAddress: String): Service {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getServices(vipAddress: String): Set<Service> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return services.filterValues {
+            it.getVipAddress() == vipAddress
+        }.values.toSet()
     }
 
     override fun handleHeartBeat(heartBeat: HeartBeat) {
@@ -25,6 +30,10 @@ class ServiceManagerImpl : ServiceManager {
     }
 
     override fun register(service: Service): Service {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val randomUUID = UUID.randomUUID()
+        service.setId(randomUUID)
+
+        services.put(randomUUID, service)
+        return service
     }
 }
