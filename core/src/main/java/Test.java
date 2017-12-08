aef4204421dda0b08ce08c15179061e3ac394ad5
Felix Klauke <info@felix-klauke.de>
2017-12-08 08:28:56 +0100

    Remove ugly test class
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- core/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Felix Klauke <fklauke@itemis.de>
- */
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println("abc" == new String("abc"));
-    }
-}
