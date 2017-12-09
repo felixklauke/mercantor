3dbacc9493c4ddc0e3818ad0e292f9785e9720cb
Felix Klauke <info@felix-klauke.de>
2017-12-09 18:13:29 +0100

    Migrate from Maven to Gradle.

--- client/src/main/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImpl.kt
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImpl.kt
@@ -7,6 +7,7 @@ import de.d3adspace.mercantor.commons.codec.ServiceMessageBodyReader
 import de.d3adspace.mercantor.commons.codec.ServiceMessageBodyWriter
 import de.d3adspace.mercantor.commons.model.service.ServiceClusterModel
 import de.d3adspace.mercantor.commons.model.service.ServiceModel
+import java.nio.ByteBuffer
 import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.client.WebTarget
 import javax.ws.rs.core.MediaType
