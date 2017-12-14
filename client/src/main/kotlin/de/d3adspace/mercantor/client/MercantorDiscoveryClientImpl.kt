c6d7b380299788ce3b9bdde4d5935f113f66d55a
Felix Klauke <info@felix-klauke.de>
2017-12-14 09:53:47 +0100

    Add basic impl and factory for client. (#100)
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImpl.kt
@@ -0,0 +1,10 @@
+package de.d3adspace.mercantor.client
+
+import de.d3adspace.mercantor.client.config.MercantorDiscoveryClientConfig
+
+class MercantorDiscoveryClientImpl(private val mercantorDiscoveryClientConfig: MercantorDiscoveryClientConfig) : MercantorDiscoveryClient {
+    
+    override fun discoverService(vipAddress: String) {
+
+    }
+}
