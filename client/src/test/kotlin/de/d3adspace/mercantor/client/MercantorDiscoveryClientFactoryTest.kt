b5315ea1e39c6e645ef8f4d8329321b959e15756
Felix Klauke <info@felix-klauke.de>
2017-12-14 10:52:09 +0100

    Feature/client infrastructure (#101)
    * Add basic impl and factory for client.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add test dependencies.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add test classes for client infrastructure.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ client/src/test/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientFactoryTest.kt
@@ -0,0 +1,23 @@
+package de.d3adspace.mercantor.client
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+class MercantorDiscoveryClientFactoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun createDiscoveryClient() {
+    }
+}
---
+++ client/src/test/kotlin/de/d3adspace/mercantor/client/config/MercantorDiscoveryClientConfigTest.kt
@@ -0,0 +1,18 @@
+package de.d3adspace.mercantor.client.config
+
+import org.junit.After
+import org.junit.Before
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+class MercantorDiscoveryClientConfigTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
