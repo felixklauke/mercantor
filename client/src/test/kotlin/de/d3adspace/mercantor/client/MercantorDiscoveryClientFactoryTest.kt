acb7314d1c32714a7c9282ee67a5c0920ccc9ffe
Felix Klauke <info@felix-klauke.de>
2017-12-14 16:51:56 +0100

    Use dummy instead of mock.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/test/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientFactoryTest.kt
+++ client/src/test/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientFactoryTest.kt
@@ -3,14 +3,13 @@ package de.d3adspace.mercantor.client
 import de.d3adspace.mercantor.client.config.MercantorDiscoveryClientConfig
 import org.junit.Assert.assertNotNull
 import org.junit.Test
-import org.mockito.Mock
 
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
 class MercantorDiscoveryClientFactoryTest {
 
-    @Mock lateinit var config: MercantorDiscoveryClientConfig
+    private val config: MercantorDiscoveryClientConfig = MercantorDiscoveryClientConfig()
 
     @Test
     fun createDiscoveryClient() {
