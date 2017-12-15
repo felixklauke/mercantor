acf1d37a61860dad1259a1be0a4592df0e2001a8
Felix Klauke <info@felix-klauke.de>
2017-12-15 10:51:40 +0100

    Remove dumb test.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/test/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImplTest.kt
+++ client/src/test/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImplTest.kt
@@ -1,8 +1,6 @@
 package de.d3adspace.mercantor.client
 
 import de.d3adspace.mercantor.client.config.MercantorDiscoveryClientConfig
-import org.junit.Test
-import java.net.ConnectException
 
 /**
  * @author Felix Klauke <fklauke@itemis.de>
@@ -10,9 +8,4 @@ import java.net.ConnectException
 class MercantorDiscoveryClientImplTest {
 
     private val discoveryClient = MercantorDiscoveryClientFactory.createDiscoveryClient(MercantorDiscoveryClientConfig())
-
-    @Test(expected = ConnectException::class)
-    fun discoverService() {
-        discoveryClient.discoverService("de.test")
-    }
 }
