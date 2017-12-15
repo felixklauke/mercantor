902f759f2ebd7f52a73d75491d2f4f4fe86ace44
Felix Klauke <info@felix-klauke.de>
2017-12-15 21:59:02 +0100

    Add full test for rest resource.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ server/src/test/kotlin/de/d3adspace/mercantor/server/resource/MercantorServerResourceTest.kt
@@ -0,0 +1,67 @@
+package de.d3adspace.mercantor.server.resource
+
+import de.d3adspace.mercantor.commons.model.HeartbeatModel
+import de.d3adspace.mercantor.commons.model.ServiceModel
+import de.d3adspace.mercantor.commons.model.ServiceStatus
+import de.d3adspace.mercantor.core.MercantorFactory
+import org.glassfish.jersey.message.internal.OutboundJaxrsResponse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.mockito.Mockito
+import java.util.*
+import javax.ws.rs.container.AsyncResponse
+
+/**
+ * @author Felix Klauke <fklaukeitemis.de>
+ */
+class MercantorServerResourceTest {
+
+    private val serviceModel = ServiceModel(UUID.randomUUID(), "test",
+            "0.0.0.0", "felix-klauke.de", 8080, "CoolerService",
+            ServiceStatus.UP, emptyMap())
+    private val asyncResponse = Mockito.mock(AsyncResponse::class.java)
+    private val mercantor = MercantorFactory.createMercantor()
+    private val mercantorResource = MercantorServerResource(mercantor)
+
+    @Test
+    fun registerService() {
+        mercantorResource.registerService(asyncResponse, serviceModel)
+
+        assertTrue(mercantor.serviceExists(serviceModel.instanceId))
+        Mockito.verify(asyncResponse).resume(Mockito.any(OutboundJaxrsResponse::class.java))
+    }
+
+    @Test
+    fun handleHeartBeat() {
+        mercantorResource.handleHeartBeat(asyncResponse, HeartbeatModel(ServiceStatus.UP, UUID.randomUUID()))
+
+        mercantor.registerService(serviceModel)
+
+        mercantorResource.handleHeartBeat(asyncResponse, HeartbeatModel(ServiceStatus.UP, serviceModel.instanceId))
+
+        Mockito.verify(asyncResponse, Mockito.times(2)).resume(Mockito.any(OutboundJaxrsResponse::class.java))
+    }
+
+    @Test
+    fun invalidateService() {
+        mercantorResource.invalidateService(asyncResponse, UUID.randomUUID())
+
+        mercantor.registerService(serviceModel)
+
+        mercantorResource.invalidateService(asyncResponse, serviceModel.instanceId)
+
+        Mockito.verify(asyncResponse, Mockito.times(2)).resume(Mockito.any(OutboundJaxrsResponse::class.java))
+
+    }
+
+    @Test
+    fun getServices() {
+        mercantorResource.getServices(asyncResponse, "test")
+
+        mercantor.registerService(serviceModel)
+
+        mercantorResource.getServices(asyncResponse, "test")
+
+        Mockito.verify(asyncResponse, Mockito.times(2)).resume(Mockito.any(OutboundJaxrsResponse::class.java))
+    }
+}
