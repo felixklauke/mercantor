24d908f8c1c55ec61cd3f3dbbdf68e9abf16e3e6
Felix Klauke <info@felix-klauke.de>
2017-12-15 14:45:44 +0100

    Remove unneeded constructor.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/main/kotlin/de/d3adspace/mercantor/client/exception/NoSuchServiceException.kt
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/exception/NoSuchServiceException.kt
@@ -3,15 +3,4 @@ package de.d3adspace.mercantor.client.exception
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
-class NoSuchServiceException : RuntimeException {
-
-    constructor()
-
-    constructor(message: String) : super(message)
-
-    constructor(message: String, cause: Throwable) : super(message, cause)
-
-    constructor(cause: Throwable) : super(cause)
-
-    constructor(message: String, cause: Throwable, enableSuppression: Boolean, writableStackTrace: Boolean) : super(message, cause, enableSuppression, writableStackTrace)
-}
+class NoSuchServiceException(message: String) : RuntimeException(message)
