78e96248216547071379645b8579f01c1830bc9a
Felix Klauke <info@felix-klauke.de>
2017-12-15 10:47:02 +0100

    Implement service loading mechanism.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/main/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImpl.kt
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImpl.kt
@@ -4,10 +4,12 @@ import de.d3adspace.mercantor.client.config.MercantorDiscoveryClientConfig
 import de.d3adspace.mercantor.client.exception.NoSuchServiceException
 import de.d3adspace.mercantor.client.util.RoundRobinList
 import de.d3adspace.mercantor.commons.model.ServiceModel
+import java.util.concurrent.ConcurrentHashMap
 
 class MercantorDiscoveryClientImpl(private val mercantorDiscoveryClientConfig: MercantorDiscoveryClientConfig) : MercantorDiscoveryClient {
 
-    private val currentServices = emptyMap<String, RoundRobinList<ServiceModel>>()
+    private val serviceLoader: ServiceLoader = ServiceLoader()
+    private val currentServices: MutableMap<String, RoundRobinList<ServiceModel>> = ConcurrentHashMap()
 
     override fun discoverService(vipAddress: String): ServiceModel {
         if (!currentServices.containsKey(vipAddress)) {
@@ -24,6 +26,9 @@ class MercantorDiscoveryClientImpl(private val mercantorDiscoveryClientConfig: M
     }
 
     private fun fetchServices(vipAddress: String) {
+        val services: List<ServiceModel> = serviceLoader.loadServices(vipAddress)
 
+        val roundRobinList = RoundRobinList(services.toMutableList())
+        currentServices.put(vipAddress, roundRobinList)
     }
 }
