95905aaf781f6190cb96edf02ecc043429be2552
Felix Klauke <info@felix-klauke.de>
2017-12-15 10:49:29 +0100

    Add test dummy classes.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ core/src/test/kotlin/de/d3adspace/mercantor/core/service/ServiceRepositoryImplTest.kt
@@ -0,0 +1,38 @@
+package de.d3adspace.mercantor.core.service
+
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author Felix Klauke <fklauke></fklauke>@itemis.de>
+ */
+class ServiceRepositoryImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getServiceExpiration() {
+    }
+
+    @Test
+    fun exists() {
+    }
+
+    @Test
+    fun register() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun updateStatus() {
+    }
+
+    @Test
+    fun getService() {
+    }
+}
