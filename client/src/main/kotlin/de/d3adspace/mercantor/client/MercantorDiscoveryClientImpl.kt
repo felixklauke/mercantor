7e66c2948f69ccec49f0dfcd7d5a4bb98b23367f
Felix Klauke <info@felix-klauke.de>
2017-12-15 13:24:14 +0100

    Add extended client logging.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/main/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImpl.kt
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/MercantorDiscoveryClientImpl.kt
@@ -4,15 +4,21 @@ import de.d3adspace.mercantor.client.config.MercantorDiscoveryClientConfig
 import de.d3adspace.mercantor.client.exception.NoSuchServiceException
 import de.d3adspace.mercantor.client.util.RoundRobinList
 import de.d3adspace.mercantor.commons.model.ServiceModel
+import org.slf4j.LoggerFactory
 import java.util.concurrent.ConcurrentHashMap
 
 class MercantorDiscoveryClientImpl(mercantorDiscoveryClientConfig: MercantorDiscoveryClientConfig) : MercantorDiscoveryClient {
+
+    private val logger = LoggerFactory.getLogger(MercantorDiscoveryClientImpl::class.java)
 
     private val serviceLoader: ServiceLoader = ServiceLoader(mercantorDiscoveryClientConfig)
     private val currentServices: MutableMap<String, RoundRobinList<ServiceModel>> = ConcurrentHashMap()
 
     override fun discoverService(vipAddress: String): ServiceModel {
+        logger.info("Discovering service for $vipAddress.")
+
         if (!currentServices.containsKey(vipAddress)) {
+            logger.info("Didn't find a local copy of services for $vipAddress.")
             fetchServices(vipAddress)
         }
 
@@ -26,8 +32,13 @@ class MercantorDiscoveryClientImpl(mercantorDiscoveryClientConfig: MercantorDisc
     }
 
     private fun fetchServices(vipAddress: String) {
+        logger.info("Beginning fetching from remote for $vipAddress.")
+
         val serviceContainer = ServiceContainer(vipAddress, serviceLoader)
-        serviceContainer.services.subscribe({
+        serviceContainer.services
+                .subscribe({
+                    logger.info("Got an update for $vipAddress.")
+
             if (!currentServices.containsKey(vipAddress)) {
                 val roundRobinList = RoundRobinList(it.toMutableList())
                 currentServices.put(vipAddress, roundRobinList)
--- client/src/main/kotlin/de/d3adspace/mercantor/client/ServiceContainer.kt
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/ServiceContainer.kt
@@ -3,12 +3,15 @@ package de.d3adspace.mercantor.client
 import de.d3adspace.mercantor.commons.model.ServiceModel
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
+import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
 
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
 class ServiceContainer(private val vipAddress: String, private val serviceLoader: ServiceLoader) {
+
+    private val logger = LoggerFactory.getLogger(ServiceContainer::class.java)
 
     private var serviceModels: List<ServiceModel> = ArrayList()
     private lateinit var serviceModelListSubject: BehaviorSubject<List<ServiceModel>>
@@ -29,15 +32,21 @@ class ServiceContainer(private val vipAddress: String, private val serviceLoader
         }
 
     private fun startPolling() {
+        logger.info("Starting to poll services from backend for $vipAddress.")
+
         val observable = Observable.interval(30, TimeUnit.SECONDS).map { fetchServices() }
         observable.subscribe(serviceModelListSubject)
     }
 
     private fun loadServices() {
+        logger.info("Initially loading services for $vipAddress.")
+
         serviceModels = fetchServices()
     }
 
     private fun fetchServices(): List<ServiceModel> {
+        logger.info("Fetching services for $vipAddress from backend.")
+
         return serviceLoader.loadServices(vipAddress).blockingLast()
     }
 }
