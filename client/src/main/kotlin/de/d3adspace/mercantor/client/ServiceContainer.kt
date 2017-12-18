57479f22fdc164a27618e6787d974375d8aecf1c
Felix Klauke <info@felix-klauke.de>
2017-12-18 16:43:54 +0100

    Add docs and modify flow for service container. (#130)
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/main/kotlin/de/d3adspace/mercantor/client/ServiceContainer.kt
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/ServiceContainer.kt
@@ -11,11 +11,24 @@ import java.util.concurrent.TimeUnit
  */
 class ServiceContainer(private val vipAddress: String, private val serviceLoader: ServiceLoader) {
 
+    /**
+     * The logger that will log all actions regarding this service.
+     */
     private val logger = LoggerFactory.getLogger(ServiceContainer::class.java)
 
+    /**
+     * All fetched service models.
+     */
     private var serviceModels: List<ServiceModel> = ArrayList()
+
+    /**
+     * Observable for the services.
+     */
     private lateinit var serviceModelListSubject: BehaviorSubject<List<ServiceModel>>
 
+    /**
+     * Initialization handling for the services.
+     */
     val services: Observable<List<ServiceModel>>
         get() {
             if (!this::serviceModelListSubject.isInitialized) {
@@ -31,22 +44,41 @@ class ServiceContainer(private val vipAddress: String, private val serviceLoader
             return serviceModelListSubject
         }
 
+    /**
+     * Start polling for a service set in the background.
+     */
     private fun startPolling() {
         logger.info("Starting to poll services from backend for $vipAddress.")
 
-        val observable = Observable.interval(30, TimeUnit.SECONDS).map { fetchServices() }
+        val observable = Observable.interval(30, TimeUnit.SECONDS).map {
+            loadServices()
+            return@map serviceModels
+        }
         observable.subscribe(serviceModelListSubject)
     }
 
+    /**
+     * Load and store the services synchronously.
+     */
     private fun loadServices() {
         logger.info("Initially loading services for $vipAddress.")
 
         serviceModels = fetchServices()
     }
 
+    /**
+     * Fetch services from backend synchronously.
+     */
     private fun fetchServices(): List<ServiceModel> {
         logger.info("Fetching services for $vipAddress from backend.")
 
-        return serviceLoader.loadServices(vipAddress).blockingLast()
+        return getServicesFromBackend().blockingLast()
+    }
+
+    /**
+     * Get the observable of the services loaded from backend.
+     */
+    private fun getServicesFromBackend(): Observable<List<ServiceModel>> {
+        return serviceLoader.loadServices(vipAddress)
     }
 }
