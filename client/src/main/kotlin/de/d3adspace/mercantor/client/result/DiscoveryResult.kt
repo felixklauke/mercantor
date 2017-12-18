998a98e310a21eff5b9fc3be9a62dcd44c1dbdb0
Felix Klauke <info@felix-klauke.de>
2017-12-18 17:49:50 +0100

    Add result wrapper for service discovery.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/result/DiscoveryResult.kt
@@ -0,0 +1,8 @@
+package de.d3adspace.mercantor.client.result
+
+import java.util.*
+
+/**
+ * @author Felix Klauke <fklauke@itemis.de>
+ */
+class DiscoveryResult(val instanceId: UUID, val hostName: String, val ipAddress: String, val port: Int)
