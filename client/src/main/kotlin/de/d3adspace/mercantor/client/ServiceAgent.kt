8509fa1b3e924163c1fc9042fd5582bfa4292910
Felix Klauke <info@felix-klauke.de>
2017-12-18 16:04:52 +0100

    Feature/service registering (#129)
    * Add service registering and unregistering.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add rest call for deletion.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Put the whole stuff together.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- client/src/main/kotlin/de/d3adspace/mercantor/client/ServiceAgent.kt
+++ client/src/main/kotlin/de/d3adspace/mercantor/client/ServiceAgent.kt
@@ -6,6 +6,7 @@ import de.d3adspace.mercantor.commons.model.HeartbeatModel
 import de.d3adspace.mercantor.commons.model.ServiceModel
 import de.d3adspace.mercantor.commons.model.ServiceStatus
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import org.glassfish.jersey.client.ClientProperties
 import org.slf4j.LoggerFactory
@@ -17,18 +18,44 @@ import javax.ws.rs.client.Entity
 import javax.ws.rs.core.MediaType
 
 /**
+ * The service agent that will take care of the local registered services.
+ *
+ * You will have to initialize the flow of the service agent using.
+ *
+ * @see #registerService() and #startHeartsbeats().
+ *
  * @author Felix Klauke <fklauke@itemis.de>
  */
 class ServiceAgent(private val mercantorDiscoveryClientConfig: MercantorDiscoveryClientConfig, private val model: ServiceModel) {
 
+    /**
+     * The logger that will log all actions.
+     */
     private val logger = LoggerFactory.getLogger(ServiceAgent::class.java)
+
+    /**
+     * The client used for network operations.
+     */
     private val client: Client = ClientBuilder.newBuilder().register(GsonJaxRSProvider()).build()
 
+    /**
+     * The dsipose bag for all subscriptions.
+     */
+    private var compositeDisposable = CompositeDisposable()
+
+    /**
+     * Initialize additional client properties.
+     */
     init {
         client.property(ClientProperties.CONNECT_TIMEOUT, 1000)
         client.property(ClientProperties.READ_TIMEOUT, 1000)
     }
 
+    /**
+     * Register the service against the backend.
+     *
+     * @return The observable of the id the service got from the backend.
+     */
     fun registerService(): Observable<UUID> {
         val responseFuture = client.target(mercantorDiscoveryClientConfig.server + "/v1/service/register")
                 .request(MediaType.APPLICATION_JSON)
@@ -40,12 +67,23 @@ class ServiceAgent(private val mercantorDiscoveryClientConfig: MercantorDiscover
         }
     }
 
+    /**
+     * Let the heart begin beating.
+     *
+     * Theh heartbeats will be sent until the service model isn't marked as UP anymore.
+     */
     fun startHeartbeats() {
-        Observable.interval(30, TimeUnit.SECONDS)
+        logger.info("Starting heartbeat background service for ${model.instanceId}.")
+
+        val subscription = Observable.interval(10, TimeUnit.SECONDS)
                 .takeUntil { model.status != ServiceStatus.UP }
-                .map { sendHeartBeat() }
+                .map { sendHeartBeat() }.subscribe()
+        compositeDisposable.add(subscription)
     }
 
+    /**
+     * Send a single heartbeat.
+     */
     private fun sendHeartBeat() {
         logger.info("Sending heartbeat for ${model.instanceId}.")
 
@@ -54,10 +92,14 @@ class ServiceAgent(private val mercantorDiscoveryClientConfig: MercantorDiscover
                 .request(MediaType.APPLICATION_JSON)
                 .async().post(Entity.json(model))
 
-        Observable.fromFuture(responseFuture, Schedulers.io())
+        val subscription = Observable.fromFuture(responseFuture, Schedulers.io())
                 .subscribe { logger.info("Sending heartbeat $model successful.") }
+        compositeDisposable.add(subscription)
     }
 
+    /**
+     * Destroy the service and the registration.
+     */
     fun destroy() {
         logger.info("Unregistering service ${model.instanceId}.")
 
@@ -67,5 +109,7 @@ class ServiceAgent(private val mercantorDiscoveryClientConfig: MercantorDiscover
                 .resolveTemplate("instanceId", model.instanceId)
                 .request(MediaType.APPLICATION_JSON)
                 .async().delete()
+
+        compositeDisposable.dispose()
     }
 }
